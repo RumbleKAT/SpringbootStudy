a0d92051817416de0ce1c9c1246c7b1b8988454d
RumbleKAT <reki318@naver.com>
2022-01-03 23:02:34 +0900

    JPQL 과 left outer join

--- src/main/java/com/rumblekat/board/repository/BoardRepository.java
+++ src/main/java/com/rumblekat/board/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package com.rumblekat.board.repository;
 
 import com.rumblekat.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     //한개의 row 오브젝트 내에 Object []로 나온다.
@@ -18,4 +22,31 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     * getBoardWithWriter()는 Board를 사용하고 있지만, Member를 같이 조회해야하는 상황
     * 내부에 있는 엔티티를 이용할 땐, Left join 뒤에 on을 이용하는 부분이 없다.
     * */
+
+    /*
+    * 특정 게시물과 해당 게시물에 속한 댓글들을 조회해야하는 상황을 생각하면 board와 reply 테이블을 조인하여 쿼리를 작성한다.
+    * select board.gno, board.title, board.writer_email, rno, text
+    * from board left outer join reply
+    *   on reply.board_gno = board.gno
+    * where board.gno = 100;
+    *
+    * */
+    @Query("SELECT b, r FROM Board b left join Reply r on r.board = b where b.gno = :bno")
+    List<Object [] > getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value= "select  b, w, count(r) " +
+        " From Board b " +
+            " Left join b.writer w "+
+            " LEFT  join Reply r on r.board = b " +
+            " group by  b ",
+            countQuery = "select count(b) from Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable); //목록 화면에 필요한 데이터
+
+    //조회 화면에 필요한 정보
+    @Query(" select b, w, count(r) from Board b left join b.writer w " +
+            " left outer join Reply r on r.board = b" +
+            " where b.gno = :bno")
+    Object getBoardByBno(@Param("bno") Long bno);
+
 }
+
--- src/test/java/com/rumblekat/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/rumblekat/board/repository/BoardRepositoryTest.java
@@ -5,9 +5,14 @@ import com.rumblekat.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -78,4 +83,31 @@ class BoardRepositoryTest {
         System.out.println("--------------------------------------");
         System.out.println(Arrays.toString(arr));
     }
+
+    @Test
+    public void testGetBoardwithReply(){
+        List<Object[]> result = boardRepository.getBoardWithReply(100L);
+
+        for(Object [] arr : result){
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+        Page<Object []> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row->{
+            Object [] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    public void testRead3(){
+        Object result = boardRepository.getBoardByBno(100L);
+        Object [] arr = (Object [])result;
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
