59baf553bd188a23ce718b7ee2341444651c84ef
RumbleKAT <reki318@naver.com>
2022-01-17 21:35:12 +0900

    init first

---
+++ src/test/java/com/rumblekat/mreivew/MreivewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumblekat.mreivew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MreivewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
