01b9aeb4aaaa3eed360957eb58577d21e69658bf
RumbleKAT <reki318@naver.com>
2022-01-18 22:33:24 +0900

    EntityGraph & @Modifying

--- src/main/java/com/rumblekat/mreivew/repository/ReviewRepository.java
+++ src/main/java/com/rumblekat/mreivew/repository/ReviewRepository.java
@@ -1,11 +1,20 @@
 package com.rumblekat.mreivew.repository;
 
+import com.rumblekat.mreivew.entity.Member;
 import com.rumblekat.mreivew.entity.Movie;
 import com.rumblekat.mreivew.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @EntityGraph(attributePaths = {"member"}, type= EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
+
+    @Modifying
+    @Query("delete  from Review mr where mr.member = :member")
+    void deleteByMember(Member member);
 }
--- src/test/java/com/rumblekat/mreivew/repository/MemberRepositoryTest.java
+++ src/test/java/com/rumblekat/mreivew/repository/MemberRepositoryTest.java
@@ -4,7 +4,9 @@ import com.rumblekat.mreivew.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
+import javax.transaction.Transactional;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +17,9 @@ class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private ReviewRepository reviewRepository;
+
     @Test
     public void insertMember(){
         IntStream.rangeClosed(1,100).forEach(i->{
@@ -24,5 +29,25 @@ class MemberRepositoryTest {
                     .nickname("reviewer"+i).build();
             memberRepository.save(member);
         });
+    }
+
+    @Transactional
+    @Commit
+    @Test
+    public void testDeleteMember(){
+        Long mid = 3L;
+        Member member = Member.builder().mid(mid).build();
+        reviewRepository.deleteByMember(member); //FK부터 제거한다.
+        memberRepository.deleteById(mid);
+
+        /*
+        * Hibernate: delete가 3번 호출되는 문제가 있음 -> @Modifying 어노테이션을 사용한다.
+    delete
+    from
+        m_member
+    where
+        mid=?
+        *
+        * */
     }
 }
